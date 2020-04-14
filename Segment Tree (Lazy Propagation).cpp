1e51f41e5f5f6d422eb854fcfa7c2a6c96625244
Shahriar Khan <58292331+shahriarkhanofficial@users.noreply.github.com>
2020-04-14 14:20:34 +0600

    Update Segment Tree (Lazy Propagation).cpp

--- Segment Tree (Lazy Propagation).cpp
+++ Segment Tree (Lazy Propagation).cpp
@@ -3,16 +3,18 @@ using namespace std ;
 
 #define ll long long
 
-struct tree 
+struct lazy_tree
 {
-	vector<ll> t, lazy;
-	void init(int a[] , int low , int n) 
+	vector<ll> t, lazy ;
+
+	void init(int a[] , int low , int n)
 	{
-		t = vector<ll> (4 * n + 4 , 0);
-		lazy = vector<ll> (4 * n + 4 , 0);
-		build(a,low,n,1) ;
+		t = vector<ll> (4 * n + 4 , 0) ;
+		lazy = vector<ll> (4 * n + 4 , 0) ;
+		build(a,1,low,n) ;
 	}
-	void build(int a[] , int low , int high , int node)
+
+	void build(int a[] , int node , int low , int high)
   	{
 		if(low==high) t[node] = a[low] ;
 		else
@@ -22,37 +24,44 @@ struct tree
 		    build(a,mid+1,high,right) ;
 		    t[node] = t[left] + t[right] ;
 		}
-        }
+    }
+
 	void upd(int node, int l, int r, ll v)
 	{
-		lazy[node] += v;
-		t[node] += (r - l + 1) * v;
+		lazy[node] += v ;
+		t[node] += (r - l + 1) * v ;
 	}
+
 	void shift(int node, int l, int r)
 	{
 		int m = (l + r)>>1 , L = node<<1 , R = L|1 ;
-		upd(L, l, m, lazy[node]);
-		upd(R, m + 1, r, lazy[node]);
-		lazy[node] = 0;
+		upd(L, l, m, lazy[node]) ;
+		upd(R, m + 1, r, lazy[node]) ;
+		lazy[node] = 0 ;
 	}
-	void update(int node, int l, int r, int i, int j, ll v) 
+
+	void update(int node, int l, int r, int i, int j, ll v)
 	{
-		if(r < i || l > j) return;
-		if(i <= l && r <= j) {
-			upd(node, l, r, v);
+		if(r < i || l > j) return ;
+		if(i <= l && r <= j)
+        {
+			upd(node, l, r, v) ;
 			return;
-		} shift(node, l, r);
+		}
+
+		shift(node, l, r);
 		int m = (l + r)>>1, L = node<<1, R = L|1 ;
-		update(L, l, m, i, j, v);
-		update(R, m + 1, r, i, j, v);
-		t[node] = t[L] + t[R];
+		update(L, l, m, i, j, v) ;
+		update(R, m + 1, r, i, j, v) ;
+		t[node] = t[L] + t[R] ;
 	}
-	ll query(int node, int l, int r, int i, int j) 
+
+	ll query(int node, int l, int r, int i, int j)
 	{
-		if(r < i || l > j) return 0;
-		if(i <= l && r <= j) return t[node];
+		if(r < i || l > j) return 0 ;
+		if(i <= l && r <= j) return t[node] ;
 		shift(node, l, r);
 		int m = (l + r)>>1, L = node<<1 , R = L|1 ;
-		return query(L, l, m, i, j) + query(R, m + 1, r, i, j);
+		return query(L, l, m, i, j) + query(R, m + 1, r, i, j) ;
 	}
-} t ;
+} ;

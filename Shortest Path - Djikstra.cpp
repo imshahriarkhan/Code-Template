574c00f4e40f8759f3b0804f19ab466d1db7a6b2
Shahriar Khan <58292331+shahriarkhanofficial@users.noreply.github.com>
2020-04-15 02:29:48 +0600

    Update Shortest Path - Djikstra.cpp

--- Shortest Path - Djikstra.cpp
+++ Shortest Path - Djikstra.cpp
@@ -1,34 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std ;
 
-#define pii pair<int,int>
-
-const int mx = 1e6 , INF = 1e9 ;
-
-vector<pii> adj[mx] ;
-
-int vis[mx] , d[mx] , n ;
-
-void djikstra(int s)
+struct shortest_path
 {
-    memset(d,INF,n) ;
-    memset(vis,0,n) ;
-    priority_queue<pii,vector<pii>,greater<pii> > pq ;
-    pq.push(s) ;
-    while(!pq.empty())
+    const int INF = 1e9 ;
+    vector<vector<pair<int,int> > > adj ;
+
+    void init(int mx)
     {
-        int from = pq.top().second ;
-        pq.pop() ;
-        if(vis[from]) continue ;
-        vis[from] = 1 ;
-        for(auto edge : adj)
+        adj = vector<vector<pair<int,int> > > (mx+1) ;
+    }
+
+    void djikstra(int s , vector<int> &d , vector<int> &p)
+    {
+        int n = adj.size() ;
+        d.assign(n,INF) ;
+        p.assign(n,-1) ;
+        d[s] = 0 ;
+        using pii = pair<int,int> ;
+        priority_queue<pii,vector<pii>, greater<pii> > q ;
+        q.push({0,s}) ;
+        while(!q.empty())
         {
-            int to = edge.first , val = edge.second ;
-            if((d[from]+val)<d[to])
+            int v = q.top().second ;
+            int d_v = q.top().first ;
+            q.pop() ;
+            if(d_v != d[v]) continue ;
+            for(auto edge : adj[v])
             {
-                d[to] = d[from] + val ;
-                pq.push({d[to],to}) ;
+                int to = edge.first , len = edge.second ;
+
+                if((d[v] + len) < d[to])
+                {
+                    d[to] = d[v] + len ;
+                    p[to] = v ;
+                    q.push({d[to],to}) ;
+                }
             }
         }
     }
-}
+} ;

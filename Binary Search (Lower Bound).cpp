8486b8a5a2a37c2466f7a47d64f0bda23d83e01d
Shahriar Khan <58292331+shahriarkhanofficial@users.noreply.github.com>
2020-05-18 21:00:00 +0600

    Update Binary Search (Lower Bound).cpp

--- Binary Search (Lower Bound).cpp
+++ Binary Search (Lower Bound).cpp
@@ -6,7 +6,7 @@ int lower_bs(int a[] , int n , int x)
     int l = 0 , h = n ;
     while(l<h)
     {
-        int mid = (l+h)>>1 ;
+        int mid = l + (h-l)/2 ;
         if(x<=a[mid]) h = mid ;
         else l = mid + 1 ;
     }

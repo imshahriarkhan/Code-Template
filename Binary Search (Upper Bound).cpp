4ba72489f2c2fbb86bcd5d0f2c3bbf0323108bb1
Shahriar Khan <58292331+shahriarkhanofficial@users.noreply.github.com>
2020-05-18 21:00:20 +0600

    Update Binary Search (Upper Bound).cpp

--- Binary Search (Upper Bound).cpp
+++ Binary Search (Upper Bound).cpp
@@ -6,14 +6,9 @@ int upper_bs(int a[] , int n , int x)
     int l = 0 , h = n ;
     while(l<h)
     {
-        int mid = (l+h)>>1 ;
+        int mid = l + (h-l)/2 ;
         if(x>=a[mid]) l = mid + 1 ;
         else h = mid ;
     }
     return l ;
-}
-
-int main()
-{
-
 }

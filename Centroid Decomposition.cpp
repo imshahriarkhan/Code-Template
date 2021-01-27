42ee07911ffa8e20d3feb8a2e69964678650728e
Shahriar Khan <58292331+shahriarkhanofficial@users.noreply.github.com>
2021-01-27 23:55:18 +0600

    Update Centroid Decomposition.cpp

--- Centroid Decomposition.cpp
+++ Centroid Decomposition.cpp
@@ -1,58 +1,109 @@
 #include<bits/stdc++.h>
+#include "factories.h"
 using namespace std ;
-
-const int mx = 2e5 + 5 ;
-
-vector<int> adj[mx] ;
-
-int subtree[mx] , vis[mx] , n , half ;
-
-void dfs(int s , int par)
+ 
+const int mx = 5e5 + 10 ;
+const long long INF = 1e18 ;
+ 
+vector<pair<int,long long> > edges[mx] ;
+ 
+int n , sub[mx] , par[mx] , depth[mx] , vis[mx] ;
+ 
+long long ans[mx] , dist[mx][20] ;
+ 
+stack<int> st ;
+ 
+void dfs(int x , int p , int d)
 {
-    int siz = adj[s].size() ;
-    subtree[s] = 1 ;
-    for(int i = 0 ; i < siz ; ++i)
+    sub[x] = 1 ;
+    for(auto i : edges[x])
     {
-        if(adj[s][i]!=par)
+        if(i.first == p) continue ;
+        if(vis[i.first]) continue ;
+        if(d) dist[i.first][d-1] = dist[x][d-1] + i.second ;
+        dfs(i.first,x,d) ;
+        sub[x] += sub[i.first] ;
+    }
+}
+ 
+int find_centroid(int x , int p , int range)
+{
+    for(auto i : edges[x])
+    {
+        if(i.first==p) continue ;
+        if(vis[i.first]) continue ;
+        if(sub[i.first]>range)
         {
-            dfs(adj[s][i],s) ;
-            subtree[s] += subtree[adj[s][i]] ;
+            return find_centroid(i.first,x,range) ;
         }
     }
+    return x ;
 }
-
-
-int centroid(int s , int par , int n)
+ 
+void build(int x , int p , int d)
 {
-    int siz = adj[s].size() ;
-    for(int i = 0 ; i < siz ; ++i)
+    dfs(x,-1,d) ;
+    int c = find_centroid(x,-1,sub[x]/2) ;
+    par[c] = p , vis[c] = 1 , depth[c] = d ;
+    for(auto i : edges[c])
     {
-        if(adj[s][i]!=par)
-        {
-            if((subtree[adj[s][i]])>n) return centroid(adj[s][i],s,n) ;
-        }
+        if(vis[i.first]) continue ;
+        dist[i.first][d] = i.second ;
+        build(i.first,c,d+1) ;
     }
-    return s ;
+    vis[c] = 0 ;
 }
-
-void solve(int c)
+ 
+void update(int x)
 {
-
-}
-
-void decomp(int s , int par)
-{
-    dfs(s,par) ;
-    int c = centroid(s,par,subtree[s]/2) ;
-    solve(c) , vis[c] = 1 ;
-    int siz = adj[c].size() ;
-    for(int i = 0 ; i < siz ; ++i)
+    int cur = x ;
+    while(cur)
     {
-        if(!vis[adj[c][i]]) decomp(adj[c][i],c) ;
+        ans[cur] = min(ans[cur],dist[x][depth[cur]]) ;
+        st.push(cur) ;
+        cur = par[cur] ;
     }
 }
-
-int main()
+ 
+long long query(int x)
 {
-
+    long long res = ans[x] ;
+    int cur = x ;
+    while(cur)
+    {
+        res = min(res,ans[cur] + dist[x][depth[cur]]) ;
+        cur = par[cur] ;
+    }
+    return res ;
+}
+ 
+void Init(int N , int A[] , int B[] , int D[])
+{
+    n = N ;
+    for(int i = 0 ; i < (N-1) ; ++i)
+    {
+        edges[A[i] + 1].push_back({B[i]+1,D[i]}) ;
+        edges[B[i] + 1].push_back({A[i]+1,D[i]}) ;
+    }
+    build(1,0,0) ;
+    for(int i = 1 ; i <= N ; ++i) ans[i] = INF ;
+}
+ 
+long long Query(int S , int X[] , int T , int Y[])
+{
+    for(int i = 0 ; i < S ; ++i)
+    {
+        update(X[i] + 1) ;
+    }
+    long long res = INF ;
+    for(int i = 0 ; i < T ; ++i)
+    {
+        res = min(res,query(Y[i] + 1)) ;
+    }
+    while(!st.empty())
+    {
+        ans[st.top()] = INF ;
+        st.pop() ;
+    }
+    return res ;
 }

ff3088c2ad6c86a855dd8bf81939a20c516125fc
Shahriar Khan <58292331+shahriarkhanofficial@users.noreply.github.com>
2021-09-03 19:12:37 +0600

    Update Persistent Segment Tree.cpp

--- Persistent Segment Tree.cpp
+++ Persistent Segment Tree.cpp
@@ -5,35 +5,63 @@ using namespace std ;
 //To quickly jump between two different versions of the Segment Tree, we need to store this roots in an array.
 //To use a specific version of the Segment Tree we simply call the query using the appropriate root vertex.
 
-struct Vertex{
-    Vertex *l , *r ;
-    int sum ;
-    Vertex(int val) : l(nullptr) , r(nullptr) , sum(val) {}
-    Vertex(Vertex *l , Vertex *r) : l(l) , r(r) , sum(0){
-        if(l) sum += l -> sum ;
-        if(r) sum += r -> sum ;
-    }
+#include<bits/stdc++.h>
+using namespace std ;
+
+const int MX = 1e5 + 5 ;
+
+struct Node
+{
+    int val ;
+    Node *left , *right ;
+
+    Node(int v) : val(v) , left(nullptr) , right(nullptr) {}
+
+    Node(Node *l_left, Node *r_right) : val(0) , left(l_left) , right(r_right) {}
 };
 
-Vertex* build(int a[] , int tl , int tr)
+int n , a[MX] ;
+
+Node* roots[MX] ;
+
+Node* build(int low , int high)
 {
-    if(tl==tr) return new Vertex(a[tl]) ;
-    int tm = (tl + tr)>>1 ;
-    return new Vertex(build(a,tl,tm),build(a,tm+1,tr)) ;
+    if(low==high) return new Node(a[low]) ;
+    int mid = (low+high)>>1 ;
+    return new Node(build(low,mid),build(mid+1,high)) ;
 }
 
-int get_sum(Vertex* v , int tl , int tr , int l , int r)
+Node* update(Node* node , int low , int high , int ind , int val)
+{
+    if(low==high) return new Node(val) ;
+    int mid = (low+high)>>1 ;
+    if(ind>mid) return new Node(node->left,update(node->right,mid+1,high,ind,val)) ;
+    else return new Node(update(node->left,low,mid,ind,val),node->right) ;
+}
+
+int query(Node* node , int low , int high , int l , int r)
 {
     if(l>r) return 0 ;
-    if(l==tl && tr==r) return v->sum ;
-    int tm = (tl + tr)>>1 ;
-    return get_sum(v->l , tl , tm , l , min(r,tm)) + get_sum(v->r,tm+1,tr,max(l,tm+1),r) ;
+    if((l>high) || (r<low)) return 0 ;
+    if((l<=low) && (high<=r))
+    {
+        return node -> val ;
+    }
+    int mid = (low+high)>>1 ;
+    return query(node->left,low,mid,l,r) + query(node->right,mid+1,high,l,r) ;
 }
 
-Vertex* update(Vertex* v , int tl , int tr , int pos , int new_val)
+void update_item(int ind , int val , int prev_time , int cur_time)
 {
-    if(tl==tr) return new Vertex(new_val) ;
-    int tm = (tl + tr)>>1 ;
-    if(pos <= tm) return new Vertex(update(v->l,tl,tm,pos,new_val),v->r) ;
-    else return new Vertex(v->l,update(v->r,tm+1,tr,pos,new_val)) ;
+    roots[cur_time] = update(roots[prev_time],1,n,ind,val) ;
+}
+
+void init(int nn , int* in)
+{
+    n = nn ;
+    for(int i = 0 ; i < n ; ++i)
+    {
+        a[i] = in[i] ;
+    }
+    roots[0] = build(1,n) ;
 }

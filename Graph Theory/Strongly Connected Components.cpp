270e2cc129794c20badd070fe3b5980296835fad
Shahriar Khan <58292331+ShahriarKhanCodes@users.noreply.github.com>
2021-12-10 18:45:25 +0600

    Create Strongly Connected Components.cpp

--- Graph Theory/Strongly Connected Components.cpp
+++ Graph Theory/Strongly Connected Components.cpp
@@ -1,44 +1,93 @@
 #include<bits/stdc++.h>
 using namespace std ;
 
-const int maxn = 1e5 + 10 ;
-
-vector<int> adj[maxn] , trans[maxn] ;
-
-int col[maxn] , vis[maxn] , idx = 0 , n , m ;
-
-stack<int> st ;
-
-void dfs(int u)
+struct node
 {
-    vis[u] = 1 ;
-    for(int v : adj[u]) if(!vis[v]) dfs(v) ;
-    st.push(u) ;
-}
+    int to = 0 , weight = 0 ;
+};
 
-void dfs2(int u)
+struct scc
 {
-    col[u] = idx ;
-    for(int v : trans[u]) if(!col[v]) dfs2(v) ;
-}
-
-void scc()
-{
-    for(int i = 1 ; i <= n ; ++i)
+    //trans is the reverse graph of adj
+    //color starts from 1
+    //vals is the values of the nodes
+    vector<vector<node> > adj , trans , cadj ;
+    vector<int> color , vis , cvis , total ;
+    stack<int> order , corder ;
+    int n , m , idx = 0 ;
+    scc(int _n , int _m , vector<vector<node> > &_adj , vector<int> &vals)
     {
-        if(!vis(i)) dfs(i) ;
+        n = _n , m = _m ;
+        color = vector<int> (n+5 , 0) ;
+        vis = vector<int> (n+5 , 0) ;
+        total = vals ;
+        adj = _adj ;
+        trans.resize(n+5) ;
     }
-    for(int u = 1 ; u <= n ; ++u)
+    void dfs(int u)     //order of the original graph
     {
-        for(int v : adj[u])
+        vis[u] = 1 ;
+        for(node v : adj[u])
         {
-            trans[v].push_back(u) ;
+            if(vis[v.to]) continue ;
+            dfs(v.to) ;
+        }
+        order.push(u) ;
+    }
+    void dfs2(int u)
+    {
+        color[u] = idx ;
+        for(node v : trans[u])
+        {
+            if(color[v.to]) continue ;
+            dfs2(v.to) ;
         }
     }
-    while(!st.empty())
+    void dfs3(int u)    //order of the compressed graph
     {
-        int u = st.top() ; st.pop() ;
-        if(col[u]) continue ;
-        ++idx , dfs2(u) ;
+        cvis[u] = 1 ;
+        for(node v : cadj[u])
+        {
+            if(cvis[v.to]) continue ;
+            dfs3(v.to) ;
+        }
+        corder.push(u) ;
     }
-}
+    void solve()
+    {
+        for(int i = 1 ; i <= n ; ++i)
+        {
+            if(!vis[i]) dfs(i) ;
+        }
+        for(int u = 1 ; u <= n ; ++u)
+        {
+            for(node v : adj[u])
+            {
+                trans[v.to].push_back({u,0}) ;
+            }
+        }
+        while(!order.empty())
+        {
+            int u = order.top() ;
+            order.pop() ;
+            if(color[u]) continue ;
+            ++idx , dfs2(u) ;
+        }
+    }
+    void compress()
+    {
+        cvis = vector<int> (n+5 , 0) ;
+        cadj.resize(n+5) ;
+        for(int u = 1 ; u <= n ; ++u)
+        {
+            for(node v : adj[u])
+            {
+                if(color[u]!=color[v.to])
+                {
+                    cadj[color[u]].push_back({color[v.to],v.weight}) ;
+                }
+                else total[color[u]] += v.weight ;
+            }
+        }
+    }
+};

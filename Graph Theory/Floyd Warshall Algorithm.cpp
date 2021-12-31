e19ce5469dcc6cacb85f0d38f8a6c4937f26df8a
Shahriar Khan <58292331+ShahriarKhanCodes@users.noreply.github.com>
2021-12-31 11:46:39 +0600

    Update Floyd Warshall Algorithm.cpp

--- Graph Theory/Floyd Warshall Algorithm.cpp
+++ Graph Theory/Floyd Warshall Algorithm.cpp
@@ -1,16 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std ;
 
+const long long INF = 1e18 + 5 ;
+
+const int MX = 505 ;
+
+long long dp[MX][MX] ;
+
 int main()
 {
-    for(int k = 1 ; k <= n ; ++k)
+    int n , m , q ;
+    scanf("%d%d%d",&n,&m,&q) ;
+    for(int i = 0 ; i <= n ; ++i)
     {
-        for(int i = 1 ; i <= n ; ++i)
+        for(int j = 0 ; j <= n ; ++j)
         {
-            for(int j = 1 ; j <= n ; ++j)
+            dp[i][j] = INF ;
+        }
+        dp[i][i] = 0 ;
+    }
+    while(m--)
+    {
+        int a , b ;
+        long long c ;
+        scanf("%d%d%lld",&a,&b,&c) ;
+        dp[a][b] = min(dp[a][b],c) ;
+        dp[b][a] = min(dp[b][a],c) ;
+    }
+    for(int k = 0 ; k <= n ; ++k)
+    {
+        for(int i = 0 ; i <= n ; ++i)
+        {
+            if(dp[i][k]==INF) continue ;
+            for(int j = 0 ; j <= n ; ++j)
             {
+                if(dp[k][j]==INF) continue ;
                 dp[i][j] = min(dp[i][j],dp[i][k] + dp[k][j]) ;
             }
         }
     }
+    while(q--)
+    {
+        int a , b ;
+        scanf("%d%d",&a,&b) ;
+        if(dp[a][b]==INF) puts("-1") ;
+        else printf("%lld\n",dp[a][b]) ;
+    }
+    return 0 ;
 }

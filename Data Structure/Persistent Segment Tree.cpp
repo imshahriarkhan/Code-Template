f4eedd83d34b0bf8dcb7ce78c08036f18a43c32b
Shahriar Khan <58292331+ShahriarKhanCodes@users.noreply.github.com>
2022-04-28 16:27:00 +0600

    Update Persistent Segment Tree.cpp

--- Data Structure/Persistent Segment Tree.cpp
+++ Data Structure/Persistent Segment Tree.cpp
@@ -1,64 +1,105 @@
+//Problem Name : PSEGTREE - Make Versions in Segment Tree
+//Link : https://www.spoj.com/problems/PSEGTREE/
+
 #include<bits/stdc++.h>
 using namespace std ;
 
-//For each modification of the Segment Tree we will receive a new root vertex.
-//To quickly jump between two different versions of the Segment Tree, we need to store this roots in an array.
-//To use a specific version of the Segment Tree we simply call the query using the appropriate root vertex.
-
-const int MX = 1e5 + 5 ;
-
 struct Node
 {
-    int val ;
-    Node *left , *right ;
-
-    Node(int v) : val(v) , left(nullptr) , right(nullptr) {}
-
-    Node(Node *l_left, Node *r_right) : val(0) , left(l_left) , right(r_right) {}
+    int left = 0 , right = 0 ;
+    long long val = 0 ;
 };
 
-int n , a[MX] ;
-
-Node* roots[MX] ;
-
-Node* build(int low , int high)
+struct psegtree
 {
-    if(low==high) return new Node(a[low]) ;
-    int mid = (low+high)>>1 ;
-    return new Node(build(low,mid),build(mid+1,high)) ;
-}
-
-Node* update(Node* node , int low , int high , int ind , int val)
-{
-    if(low==high) return new Node(val) ;
-    int mid = (low+high)>>1 ;
-    if(ind>mid) return new Node(node->left,update(node->right,mid+1,high,ind,val)) ;
-    else return new Node(update(node->left,low,mid,ind,val),node->right) ;
-}
-
-int query(Node* node , int low , int high , int l , int r)
-{
-    if(l>r) return 0 ;
-    if((l>high) || (r<low)) return 0 ;
-    if((l<=low) && (high<=r))
+    int sz = 0 , cnt = 0 ;
+    vector<Node> T ;
+    vector<int> root ;
+    void init(int n , int m , long long a[])
     {
-        return node -> val ;
+        //as n <= 1e5, I have used 100*n...
+        T = vector<Node> (100*n + 100) ;
+        root = vector<int> (m + 10 , 0) ;
+        root[0] = build(a,++sz,1,n) ;
     }
-    int mid = (low+high)>>1 ;
-    return query(node->left,low,mid,l,r) + query(node->right,mid+1,high,l,r) ;
-}
-
-void update_item(int ind , int val , int prev_time , int cur_time)
-{
-    roots[cur_time] = update(roots[prev_time],1,n,ind,val) ;
-}
-
-void init(int nn , int* in)
-{
-    n = nn ;
-    for(int i = 0 ; i < n ; ++i)
+    int build(long long a[] , int node , int low , int high)
     {
-        a[i] = in[i] ;
+        if(low==high)
+        {
+            T[node].val = a[low] ;
+            return node ;
+        }
+        int mid = (low+high)>>1 ;
+        T[node].left = build(a,++sz,low,mid) ;
+        T[node].right = build(a,++sz,mid+1,high) ;
+        T[node].val = (T[T[node].left].val+T[T[node].right].val) ;
+        return node ;
     }
-    roots[0] = build(1,n) ;
+    int update_(int node , int low , int high , int idx , long long val)
+    {
+        if(low==high)
+        {
+            ++sz ;
+            T[sz].val = T[node].val + val ;
+            return sz ;
+        }
+        int mid = (low+high)>>1 , cur = ++sz ;
+        if(idx<=mid) T[cur].left = update_(T[node].left,low,mid,idx,val) , T[cur].right = T[node].right ;
+        else T[cur].left = T[node].left , T[cur].right = update_(T[node].right,mid+1,high,idx,val) ;
+        T[cur].val = (T[T[cur].left].val+T[T[cur].right].val) ;
+        return cur ;
+    }
+    void update(int f , int low , int high , int idx , long long val)
+    {
+        ++cnt ;
+        root[cnt] = update_(root[f],low,high,idx,val) ;
+    }
+    long long query_(int node , int low , int high , int l , int r)
+    {
+        if((r<low) || (l>high)) return 0 ;
+        if((l<=low) && (high<=r))
+        {
+            return T[node].val ;
+        }
+        int mid = (low+high)>>1 ;
+        return (query_(T[node].left,low,mid,l,r)+query_(T[node].right,mid+1,high,l,r)) ;
+    }
+    long long query(int x , int low , int high , int l , int r)
+    {
+        return query_(root[x],low,high,l,r) ;
+    }
+};
+
+int main()
+{
+    int n ;
+    scanf("%d",&n) ;
+    long long a[n+2] ;
+    for(int i = 1 ; i <= n ; ++i)
+    {
+        scanf("%lld",&a[i]) ;
+    }
+    int q ;
+    scanf("%d",&q) ;
+    psegtree p ;
+    p.init(n,q,a) ;
+    for(int i = 1 ; i <= q ; ++i)
+    {
+        int type ;
+        scanf("%d",&type) ;
+        if(type==1)
+        {
+            int f , idx ;
+            long long val ;
+            scanf("%d%d%lld",&f,&idx,&val) ;
+            p.update(f,1,n,idx,val) ;
+        }
+        else
+        {
+            int x , l , r ;
+            scanf("%d%d%d",&x,&l,&r) ;
+            printf("%lld\n",p.query(x,1,n,l,r)) ;
+        }
+    }
+    return 0 ;
 }

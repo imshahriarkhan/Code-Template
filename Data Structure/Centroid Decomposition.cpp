ace8f3fbc6846553587058066ffb9bfd2884afa2
Shahriar Khan <58292331+ShahriarKhanOme@users.noreply.github.com>
2022-05-20 21:33:36 +0600

    Update Centroid Decomposition.cpp

--- Data Structure/Centroid Decomposition.cpp
+++ Data Structure/Centroid Decomposition.cpp
@@ -1,109 +1,148 @@
+//Problem Name : Xenia and Tree
+//Problem Link : https://codeforces.com/contest/342/problem/E
+
 #include<bits/stdc++.h>
-#include "factories.h"
 using namespace std ;
- 
-const int mx = 5e5 + 10 ;
-const long long INF = 1e18 ;
- 
-vector<pair<int,long long> > edges[mx] ;
- 
-int n , sub[mx] , par[mx] , depth[mx] , vis[mx] ;
- 
-long long ans[mx] , dist[mx][20] ;
- 
-stack<int> st ;
- 
-void dfs(int x , int p , int d)
+
+const int MX = 1e5 + 5 , LG = 25 , INF = 1e9 + 5 ;
+
+struct centroid_tree
 {
-    sub[x] = 1 ;
-    for(auto i : edges[x])
+    int n = 0 , timer = 0 ;
+    vector<int> par , cost , sub , centroid , tin , tout , depth ;
+    vector<vector<int> > adj , up ;
+    void init(int N , vector<int> f[])
     {
-        if(i.first == p) continue ;
-        if(vis[i.first]) continue ;
-        if(d) dist[i.first][d-1] = dist[x][d-1] + i.second ;
-        dfs(i.first,x,d) ;
-        sub[x] += sub[i.first] ;
-    }
-}
- 
-int find_centroid(int x , int p , int range)
-{
-    for(auto i : edges[x])
-    {
-        if(i.first==p) continue ;
-        if(vis[i.first]) continue ;
-        if(sub[i.first]>range)
+        n = N ;
+        par = vector<int> (N+2,0) ;
+        cost = vector<int> (N+2,INF) ;
+        sub = vector<int> (N+2,0) ;
+        adj = vector<vector<int> > (N+2) ;
+        centroid = vector<int> (N+2,0) ;
+        tin = vector<int> (N+2,0) ;
+        tout = vector<int> (N+2,0) ;
+        depth = vector<int> (N+2,0) ;
+        up = vector<vector<int> > (N+2,vector<int>(LG+1,0)) ;
+        for(int i = 1 ; i <= N ; ++i)
         {
-            return find_centroid(i.first,x,range) ;
+            for(int j : f[i])
+            {
+                adj[i].push_back(j) ;
+            }
+        }
+        build_tree(1,0) ;
+        dfs2(1,1) ;
+    }
+    void dfs(int s , int p)
+    {
+        sub[s] = 1 ;
+        for(int t : adj[s])
+        {
+            if(t==p) continue ;
+            if(centroid[t]) continue ;
+            dfs(t,s) ;
+            sub[s] += sub[t] ;
         }
     }
-    return x ;
-}
- 
-void build(int x , int p , int d)
+    int find_centroid(int s , int p , int x)
+    {
+        for(int t : adj[s])
+        {
+            if(t==p) continue ;
+            if(centroid[t]) continue ;
+            if(sub[t]>(x/2)) return find_centroid(t,s,x) ;
+        }
+        return s ;
+    }
+    void build_tree(int s , int p)
+    {
+        dfs(s,p) ;
+        int C = find_centroid(s,p,sub[s]) ;
+        centroid[C] = 1 , par[C] = p ;
+        for(int t : adj[C])
+        {
+            if(centroid[t]) continue ;
+            build_tree(t,C) ;
+        }
+    }
+    void dfs2(int s , int p)
+    {
+        up[s][0] = p , tin[s] = ++timer , depth[s] = depth[p] + 1 ;
+        for(int i = 1 ; i < LG ; ++i)
+        {
+            up[s][i] = up[up[s][i-1]][i-1] ;
+        }
+        for(int t : adj[s])
+        {
+            if(t==p) continue ;
+            dfs2(t,s) ;
+        }
+        tout[s] = ++timer ;
+    }
+    int is_ancestor(int u , int v)
+    {
+        return ((tin[u]<=tin[v]) && (tout[u]>=tout[v])) ;
+    }
+    int find_lca(int u , int v)
+    {
+        if(is_ancestor(u,v)) return u ;
+        if(is_ancestor(v,u)) return v ;
+        for(int i = LG - 1 ; i >= 0 ; --i)
+        {
+            if(!is_ancestor(up[u][i],v))
+            {
+                u = up[u][i] ;
+            }
+        }
+        return up[u][0] ;
+    }
+    int dist(int u , int v)
+    {
+        int lca = find_lca(u,v) ;
+        return depth[u] + depth[v] - (2*depth[lca]) ;
+    }
+    void update(int u)
+    {
+        int org = u ;
+        while(u)
+        {
+            cost[u] = min(cost[u],dist(org,u)) ;
+            u = par[u] ;
+        }
+    }
+    int query(int u)
+    {
+        int ret = INF , org = u ;
+        while(u)
+        {
+            ret = min(ret,cost[u] + dist(org,u)) ;
+            u = par[u] ;
+        }
+        return ret ;
+    }
+};
+
+int main()
 {
-    dfs(x,-1,d) ;
-    int c = find_centroid(x,-1,sub[x]/2) ;
-    par[c] = p , vis[c] = 1 , depth[c] = d ;
-    for(auto i : edges[c])
+    int n , m ;
+    scanf("%d%d",&n,&m) ;
+    vector<int> adj[MX] ;
+    for(int i = 1 ; i < n ; ++i)
     {
-        if(vis[i.first]) continue ;
-        dist[i.first][d] = i.second ;
-        build(i.first,c,d+1) ;
+        int a , b ;
+        scanf("%d%d",&a,&b) ;
+        adj[a].push_back(b) ;
+        adj[b].push_back(a) ;
     }
-    vis[c] = 0 ;
-}
- 
-void update(int x)
-{
-    int cur = x ;
-    while(cur)
+    centroid_tree C ;
+    C.init(n,adj) ;
+    C.update(1) ;
+    while(m--)
     {
-        ans[cur] = min(ans[cur],dist[x][depth[cur]]) ;
-        st.push(cur) ;
-        cur = par[cur] ;
+        int t , v ;
+        scanf("%d%d",&t,&v) ;
+        if(t==1) C.update(v) ;
+        else printf("%d\n",C.query(v)) ;
     }
-}
- 
-long long query(int x)
-{
-    long long res = ans[x] ;
-    int cur = x ;
-    while(cur)
-    {
-        res = min(res,ans[cur] + dist[x][depth[cur]]) ;
-        cur = par[cur] ;
-    }
-    return res ;
-}
- 
-void Init(int N , int A[] , int B[] , int D[])
-{
-    n = N ;
-    for(int i = 0 ; i < (N-1) ; ++i)
-    {
-        edges[A[i] + 1].push_back({B[i]+1,D[i]}) ;
-        edges[B[i] + 1].push_back({A[i]+1,D[i]}) ;
-    }
-    build(1,0,0) ;
-    for(int i = 1 ; i <= N ; ++i) ans[i] = INF ;
-}
- 
-long long Query(int S , int X[] , int T , int Y[])
-{
-    for(int i = 0 ; i < S ; ++i)
-    {
-        update(X[i] + 1) ;
-    }
-    long long res = INF ;
-    for(int i = 0 ; i < T ; ++i)
-    {
-        res = min(res,query(Y[i] + 1)) ;
-    }
-    while(!st.empty())
-    {
-        ans[st.top()] = INF ;
-        st.pop() ;
-    }
-    return res ;
+    return 0 ;
 }
